dc1cd773b07e29f86f574848f0bb2061d58a3863
Karamo Kanagi <kamskanagi@hotmail.com>
2023-05-18 10:29:54 +0800

    Update Nav.js

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -14,15 +14,7 @@ export default function Nav() {
 
           
       </div>
-      <div className='headerTitle'>
-            <span className='headerTitleSm'> Nextjs & Strapi </span>
-            <span className='headerTitleLg'> Blog</span>
-        </div>
-      <img
-          className="headerImg"
-          src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt=""
-        />
+     
       </div>
   )
 }
